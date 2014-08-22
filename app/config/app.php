cfeb50f0e194dfa805b6215fb28e496f9e10566d
raulghm <raulghm@gmail.com>
2014-08-21 22:49:55 -0400

    ..

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
 		'Themonkeys\Cachebuster\CachebusterServiceProvider',
+		'CanGelis\L4pdf\ServiceProvider'
 	),
 
 	/*
